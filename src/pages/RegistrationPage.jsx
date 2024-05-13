2eb1486c28258f1683939829f7cc781f21ca9369
AVUKU-PRAGATHESWARI <117664019+AVUKU-PRAGATHESWARI@users.noreply.github.com>
2024-05-13 11:01:11 +0530

    Styled the register page

--- src/pages/RegistrationPage.jsx
+++ src/pages/RegistrationPage.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Select, Input } from "antd";
 import { Skills } from "../constants/Skills";
-
+import "../styles/Login.css";
 // import '../../styles/freelancer/register.css'
 
 const RegistrationPage = ({
@@ -22,64 +22,68 @@ const RegistrationPage = ({
   handleSubmit,
 }) => {
   return (
-    <div>
-      <form action="" className="registration" onSubmit={handleSubmit}>
-        <Input
-          type="text"
-          placeholder="Name of the User"
-          className="feild"
-          value={name}
-          onChange={handleNameChange}
-        />
-        <Input
-          type="email"
-          placeholder="Student email"
-          className="feild"
-          value={email}
-          onChange={handleEmailChange}
-        />
-        <Input
-          type="password"
-          placeholder="Password"
-          className="feild"
-          value={password}
-          onChange={handlePasswordChange}
-        />
-        <Input
-          type="text"
-          placeholder="Location"
-          className="feild"
-          value={location}
-          onChange={handleLocationChange}
-        />
-        <Select
-          mode="multiple"
-          style={{ width: "230px" }}
-          placeholder="Select skills"
-          onChange={handleSelectSkills}
-          value={selectedSkills}
-        >
-          {Skills.map((skill) => (
-            <Select.Option key={skill.value} value={skill.value}>
-              {skill.label}
-            </Select.Option>
-          ))}
-        </Select>
+    <div className="login-page">
+      <div className="login-box">
+        <form action="" className="login-form" onSubmit={handleSubmit}>
+          <Input
+            type="text"
+            placeholder="Name of the User"
+            className="input-field"
+            value={name}
+            onChange={handleNameChange}
+          />
+          <Input
+            type="email"
+            placeholder="Student email"
+            className="input-field"
+            value={email}
+            onChange={handleEmailChange}
+          />
+          <Input
+            type="password"
+            placeholder="Password"
+            className="input-field"
+            value={password}
+            onChange={handlePasswordChange}
+          />
+          <Input
+            type="text"
+            placeholder="Location"
+            className="input-field"
+            value={location}
+            onChange={handleLocationChange}
+          />
+          <Select
+            mode="multiple"
+            style={{ width: "230px" }}
+            placeholder="Select skills"
+            onChange={handleSelectSkills}
+            value={selectedSkills}
+            className="input-field"
+          >
+            {Skills.map((skill) => (
+              <Select.Option style={{ backgroundColor: "#0E1E2B", color: "#f1f4ff" }} // Adjust as needed
+              key={skill.value} value={skill.value}>
+                {skill.label}
+              </Select.Option>
+            ))}
+          </Select>
 
-        <Input
-          type="text"
-          placeholder="Describe yourself"
-          className="feild-d"
-          value={describe}
-          onChange={handleDescribeChange}
-        />
-        <Input
-          type="submit"
-          value="Submit"
-          className="feild-s"
-          style={{ backgroundColor: "rgb(250, 208, 129)" }}
-        />
-      </form>
+          <Input
+            type="text"
+            placeholder="Describe yourself"
+            className="input-field"
+            value={describe}
+            onChange={handleDescribeChange}
+          />
+          <Input
+            type="submit"
+            value="Submit"
+            className="input-field"
+            style={{ backgroundColor: "rgb(250, 208, 129)" }}
+          />
+        </form>
+      </div>
     </div>
   );
 };
